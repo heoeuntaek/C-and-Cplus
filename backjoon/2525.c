19d464c86b001de5c45ff127393dc6351173be1b
heoeuntaek <gjdmsxor@naver.com>
2023-03-13 16:10:01 +0900

    d

---
+++ backjoon/2525.c
@@ -0,0 +1,34 @@
+//#include <stdio.h>
+//int main() {
+//	int agohour = 0;
+//	int agominute = 0;
+//	int ovenminute = 0;
+//
+//	int afterhour = 0;
+//	int afterminute = 0;
+//
+//
+//	scanf("%d %d", &agohour, &agominute);
+//	scanf("%d", &ovenminute);
+//
+//
+//	afterminute = agominute + ovenminute;
+//	afterhour = agohour;
+//
+//	while (afterminute >= 60) {
+//		afterhour++;
+//		afterminute -= 60;
+//	}
+//	if (afterhour >= 24) {
+//		afterhour -= 24;
+//	}
+//
+//	printf("%d %d", afterhour, afterminute);
+//
+//
+//
+//
+//
+//
+//	return 0;
+//}
--- backjoon/2884.c
+++ backjoon/2884.c
@@ -1,24 +1,24 @@
-#include <stdio.h>
-int main() {
-	int hour, minute;
-	int agohour, agominute = 0;
-	scanf("%d %d", &hour, &minute);
-
-
-	agohour = hour;
-
-	if (minute < 45) {
-		if (hour == 0) {
-			hour += 24;
-		}
-		agohour = hour - 1;
-		minute += 60;
-
-	}
-
-
-	agominute = minute - 45;
-	printf("%d %d", agohour, agominute);
-
-	return 0;
-}
+//#include <stdio.h>
+//int main() {
+//	int hour, minute;
+//	int agohour, agominute = 0;
+//	scanf("%d %d", &hour, &minute);
+//	 
+//
+//	agohour = hour;
+//
+//	if (minute < 45) {
+//		if (hour == 0) {
+//			hour += 24;
+//		}
+//		agohour = hour - 1;
+//		minute += 60;
+//
+//	}
+//
+//
+//	agominute = minute - 45;
+//	printf("%d %d", agohour, agominute);
+//
+//	return 0;
+//}
