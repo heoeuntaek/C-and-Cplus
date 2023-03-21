22834604a8172f78cd727f83a24cb047081a58f0
heoeuntaek <gjdmsxor@naver.com>
2023-03-21 11:25:34 +0900

    d

--- backjoon/10818.c
+++ backjoon/10818.c
@@ -1,32 +1,32 @@
-﻿#include <stdio.h>
-
-int main(void) {
-	int n, input;
-	scanf("%d", &n);
-
-	int max = -1,
-		min = 10000;
-	int arr[100];
-
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &arr[i]);
-
-	}
-	max = arr[0];
-	min = arr[0];
-
-	for (int i = 0; i < n; i++) {
-
-
-		if (arr[i] < min) {
-			min = arr[i];
-		}
-		if (arr[i] > max) {
-			max = arr[i];
-		}
-	}
-
-	printf("%d %d", min, max);
-
-	return 0;
-}
+﻿//#include <stdio.h>
+//
+//int main(void) {
+//	int n, input;
+//	scanf("%d", &n);
+//
+//	int max = -1,
+//		min = 10000;
+//	int arr[100];
+//
+//	for (int i = 0; i < n; i++) {
+//		scanf("%d", &arr[i]);
+//
+//	}
+//	max = arr[0];
+//	min = arr[0];
+//
+//	for (int i = 0; i < n; i++) {
+//
+//
+//		if (arr[i] < min) {
+//			min = arr[i];
+//		}
+//		if (arr[i] > max) {
+//			max = arr[i];
+//		}
+//	}
+//
+//	printf("%d %d", min, max);
+//
+//	return 0;
+//}
