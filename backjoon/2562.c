3e8902ad75bb577c6b0305f980306b31daed6d6b
heoeuntaek <gjdmsxor@naver.com>
2023-03-22 10:26:31 +0900

    d

--- backjoon/2562.c
+++ backjoon/2562.c
@@ -1,22 +1,22 @@
-#include <stdio.h>
-
-int main() {
-	int max, index=-1;
-	int arr[9];
-	
-	for (int i = 1; i<10; i++) {
-		scanf("%d\n", &arr[i]);
-	}
-	max = arr[0];
-	for (int i = 1; i<10; i++) {
-		if (arr[i] > max) {
-			max = arr[i];
-			index = i;
-		}
-	}
-
-	printf("%d\n", max);
-	printf("%d\n", index);
-
-	return 0;
-}
+//#include <stdio.h>
+//
+//int main() {
+//	int max, index=-1;
+//	int arr[9];
+//	
+//	for (int i = 1; i<10; i++) {
+//		scanf("%d\n", &arr[i]);
+//	}
+//	max = arr[0];
+//	for (int i = 1; i<10; i++) {
+//		if (arr[i] > max) {
+//			max = arr[i];
+//			index = i;
+//		}
+//	}
+//
+//	printf("%d\n", max);
+//	printf("%d\n", index);
+//
+//	return 0;
+//}
