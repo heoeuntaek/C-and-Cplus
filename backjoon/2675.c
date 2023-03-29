86309fb868b7b65dbcd6a57af7dceb2d97f0bd44
heoeuntaek <gjdmsxor@naver.com>
2023-03-30 08:21:00 +0900

    d

---
+++ backjoon/2675.c
@@ -0,0 +1,22 @@
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main() {
+//
+//	int num;
+//	scanf("%d", &num);
+//	for (int i = 0; i < num; i++) {
+//		int mul;
+//		char s[8];
+//		scanf("%d %s", &mul, &s);
+//		for (int i = 0; i < strlen(s); i++) {
+//			for (int j = 0; j < mul; j++) {
+//				printf("%c", s[i]);
+//			}
+//			
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
