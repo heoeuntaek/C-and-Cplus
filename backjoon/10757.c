ce8de7cb0bb806585ca1f2d901656ec235739328
heoeuntaek <gjdmsxor@naver.com>
2023-04-15 18:41:48 +0900

    d

---
+++ backjoon/10757.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+
+	unsigned long long a, b;
+	scanf("%u, %u", &a, &b);
+	printf("%llu", a + b);
+	
+	return 0;
+}
---
+++ backjoon/2869.c
@@ -0,0 +1,13 @@
+//#include <stdio.h>
+//#include <math.h> 
+//
+//int main() {
+//	int a, b, v;
+//	int day = 0;
+//	int high = 0;
+//	scanf("%d %d %d", &a, &b, &v);
+//	day = (v - b - 1) / (a - b) + 1;
+//	printf("%d", day);
+//
+//	return 0;
+//}
