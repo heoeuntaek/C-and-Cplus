335c124735321773841ae7f80a1e6887c7c4ffa1
heoeuntaek <gjdmsxor@naver.com>
2023-04-23 07:23:13 +0900

    d

--- backjoon/1157.c
+++ backjoon/1157.c
@@ -1,48 +1,48 @@
-#include <stdio.h>
-#include <stdlib.h> //srand, rand를 사용하기 위한 헤더파일
-#include <time.h> // time을 사용하기 위한 헤더파일
-
-int main() {
-
-	char str[1000000];
-	scanf("%s", str);
-	/*strlwr(str);*/
-
-	
-	int alphaIndex[27] = { 0, };
-	int len = strlen(str);
-	for (int i = 0; i < len; i++) {
-		if ((str[i] >= 'A') && (str[i] <= 'Z')) {
-			str[i] = str[i] - 'A' + 'a';
-		}
-		int index = str[i] - 'a';
-		alphaIndex[index]++;
-	}
-
-	int max = alphaIndex[0];
-	int maxIndex=0;
-	for (int i = 0; i < 27; i++) {
-		if (max < alphaIndex[i]) {
-			max = alphaIndex[i];
-			maxIndex = i;
-		}
-
-	}
-	int result = 0;
-	for (int i = 0; i < 27; i++) {
-		if (max == alphaIndex[i]) {
-			result++;
-		}
-	}
-	if (result>1) {
-		printf("?");
-	}
-	else {
-		printf("%c", maxIndex + 'A');
-	}
-
-
-	return 0;
-
-
-}
+//#include <stdio.h>
+//#include <stdlib.h> //srand, rand를 사용하기 위한 헤더파일
+//#include <time.h> // time을 사용하기 위한 헤더파일
+//
+//int main() {
+//
+//	char str[1000000];
+//	scanf("%s", str);
+//	/*strlwr(str);*/
+//
+//	
+//	int alphaIndex[27] = { 0, };
+//	int len = strlen(str);
+//	for (int i = 0; i < len; i++) {
+//		if ((str[i] >= 'A') && (str[i] <= 'Z')) {
+//			str[i] = str[i] - 'A' + 'a';
+//		}
+//		int index = str[i] - 'a';
+//		alphaIndex[index]++;
+//	}
+//
+//	int max = alphaIndex[0];
+//	int maxIndex=0;
+//	for (int i = 0; i < 27; i++) {
+//		if (max < alphaIndex[i]) {
+//			max = alphaIndex[i];
+//			maxIndex = i;
+//		}
+//
+//	}
+//	int result = 0;
+//	for (int i = 0; i < 27; i++) {
+//		if (max == alphaIndex[i]) {
+//			result++;
+//		}
+//	}
+//	if (result>1) {
+//		printf("?");
+//	}
+//	else {
+//		printf("%c", maxIndex + 'A');
+//	}
+//
+//
+//	return 0;
+//
+//
+//}
---
+++ backjoon/4344.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h> //srand, rand를 사용하기 위한 헤더파일
+#include <time.h> // time을 사용하기 위한 헤더파일
+
+int main() {
+
+	int number;
+	int numberOfStudent;
+	int arrOfScore[1000] = { 0, };
+
+	scanf("%d", &number);
+	for (int i = 0; i < number; i++) {
+		scanf("%d", &numberOfStudent);
+		for (int i = 0; i < numberOfStudent; i++) {
+			scanf("%d", &arrOfScore[i]);
+		}
+		
+		int sumOfScore = 0;
+		for (int i = 0; i < numberOfStudent; i++) {
+			sumOfScore += arrOfScore[i];
+		}
+		int average= sumOfScore/numberOfStudent; 
+		int countOfUpScore = 0;
+		for (int i = 0; i < numberOfStudent; i++) {
+			if (average < arrOfScore[i]) {
+				countOfUpScore++;
+			}
+		}
+		float answer = (float)countOfUpScore / (float)numberOfStudent;
+		printf("%.3f%%\n",answer*100);
+
+		
+	}
+
+
+	return 0;
+
+
+}
