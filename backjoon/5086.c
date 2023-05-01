49df30c16ccdbc4edaddaaa95ec62a58521b3b8e
heoeuntaek <gjdmsxor@naver.com>
2023-05-02 07:30:14 +0900

    d

---
+++ backjoon/5086.c
@@ -0,0 +1,27 @@
+//#include <stdio.h>
+//#include <stdlib.h> //srand, rand를 사용하기 위한 헤더파일
+//#include <time.h> // time을 사용하기 위한 헤더파일
+//
+//
+//int main(void) {
+//	while (1) {
+//		int a, b;
+//		scanf("%d %d", &a, &b);
+//		if (a == 0) if (b == 0) return 0;
+//		if (a%b==0)//a는 b의 배수
+//		{
+//			printf("multiple\n");
+//		}
+//		else if (b % a == 0) {
+//			printf("factor\n");
+//		}
+//		else {
+//			printf("neither\n");
+//		}
+//	
+//
+//	}
+//
+//
+//	return 0;
+//}
