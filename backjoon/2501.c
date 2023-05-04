52ef853a2645e9a7a23be63812339e289bc0f345
heoeuntaek <gjdmsxor@naver.com>
2023-05-05 01:15:12 +0900

    d

--- backjoon/2501.c
+++ backjoon/2501.c
@@ -1,34 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h> //srand, rand를 사용하기 위한 헤더파일
-#include <time.h> // time을 사용하기 위한 헤더파일
-
-
-int main(void) {
-	//약수 구하기
-	int a;
-	int k;
-	scanf("%d %d", &a, &k);
-	int arr[10000] = { a+1 };
-	int b = 0;
-	for (int i = 1; i < a + 1; i++) {
-		if (a % i == 0) {  
-			arr[b] = i;
-			b++;
-
-		}
-	}
-	
-	
-	int answer = 0;
-	for (int i = 0; i < k; i++) {
-		if (arr[i] > a)continue;
-		if (arr[i] == -1) answer =0;
-		answer = arr[i];
-	}
-
-	printf("%d", answer);
-
-
-
-	return 0;
-}
+//#include <stdio.h>
+//#include <stdlib.h> //srand, rand를 사용하기 위한 헤더파일
+//#include <time.h> // time을 사용하기 위한 헤더파일
+//
+//
+//int main(void) {
+//	//약수 구하기
+//	int a;
+//	int k;
+//	scanf("%d %d", &a, &k);
+//	int arr[10000] = { a+1 };
+//	int b = 0;
+//	for (int i = 1; i < a + 1; i++) {
+//		if (a % i == 0) {  
+//			arr[b] = i;
+//			b++;
+//
+//		}
+//	}
+//	
+//	
+//	int answer = 0;
+//	for (int i = 0; i < k; i++) {
+//		if (arr[i] > a)continue;
+//		if (arr[i] == -1) answer =0;
+//		answer = arr[i];
+//	}
+//
+//	printf("%d", answer);
+//
+//
+//
+//	return 0;
+//}
---
+++ backjoon/9506.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	while (1) {
+		int a;
+		cin >> a;
+		if (a == -1)break;
+		vector<int> v;
+		for (int i = 1; i < a + 1; i++) {
+			if (a % i == 0) {
+				v.push_back(i);
+			}
+		}
+		int sum = 0;
+		for (int i = 0; i < v.size() - 1; i++) {
+			sum += v[i];
+		}
+		if (sum == a) {
+			cout << a << " = ";
+			for (int i = 0; i < v.size()-1; i++) {
+				if (i == (v.size() - 2)) {
+					cout << v[i];
+				}
+				else {
+					cout << v[i] << " + ";
+				}
+
+			}
+			cout << endl;
+		}
+		else {
+			cout << a << " is NOT perfect.";
+			cout << endl;
+
+		}
+		
+	}
+	//for (auto loop : vec) {
+	//	cout << loop;
+	//}
+	return 0;
+}
