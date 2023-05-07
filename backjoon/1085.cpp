eb97c9d8ddef71a390f5ec59a497218fefea4b4d
heoeuntaek <gjdmsxor@naver.com>
2023-05-07 18:13:24 +0900

    d

---
+++ backjoon/1085.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	//첫째 줄에 x, y, w, h가 주어진다.
+	int x, y, x2, y2;
+	cin >> x >> y >> x2 >> y2;
+
+	int result = min(min(x2 - x, y2 - y),min(x,y));
+	cout << result;
+
+
+	return 0;
+}
---
+++ backjoon/27323.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//#include <vector>
+//#include <algorithm> 
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int w, h;
+//	cin >> w >> h;
+//	cout << w * h;
+//	
+//	return 0;
+//}
