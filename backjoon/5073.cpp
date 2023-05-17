e4eb7ffa9045e3da5fc94c5010741dacdd115b35
heoeuntaek <gjdmsxor@naver.com>
2023-05-18 06:51:51 +0900

    d

--- backjoon/5073.cpp
+++ backjoon/5073.cpp
@@ -1,48 +1,48 @@
-#include <iostream>
-#include <vector>
-#include <algorithm> 
-#include<string>
-
-using namespace std;
-
-int main()
-{
-	int a, b, c;
-	while (true) {
-		cin >> a >> b >> c;
-		if (a == 0 && b == 0 && c == 0) {
-			break;
-		}
-		vector<int> v = { a,b,c };
-		int max = 0;
-
-		sort(v.begin(), v.end());
-		if (a + b <= c) {
-			cout << "Invalid" << endl;
-
-		}
-		else if (a == b &&b == c) {
-			cout << "Equilateral" << endl;
-		}
-		else if (a == b || b == c || a == c) {
-			cout << "Isosceles" << endl;
-		}
-		
-		else {
-			
-			
-				cout << "Scalene" << endl;
-			
-
-
-
-
-
-		}
-
-
-	}
-	
-
-	return 0;
-}
+//#include <iostream>
+//#include <vector>
+//#include <algorithm> 
+//#include<string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int a, b, c;
+//	while (true) {
+//		cin >> a >> b >> c;
+//		if (a == 0 && b == 0 && c == 0) {
+//			break;
+//		}
+//		vector<int> v = { a,b,c };
+//		int max = 0;
+//
+//		sort(v.begin(), v.end());
+//		if (a + b <= c) {
+//			cout << "Invalid" << endl;
+//
+//		}
+//		else if (a == b &&b == c) {
+//			cout << "Equilateral" << endl;
+//		}
+//		else if (a == b || b == c || a == c) {
+//			cout << "Isosceles" << endl;
+//		}
+//		
+//		else {
+//			
+//			
+//				cout << "Scalene" << endl;
+//			
+//
+//
+//
+//
+//
+//		}
+//
+//
+//	}
+//	
+//
+//	return 0;
+//}
