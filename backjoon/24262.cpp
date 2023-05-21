78660ceada06da08553db8c58939eaa93afea2e6
heoeuntaek <gjdmsxor@naver.com>
2023-05-21 21:21:25 +0900

    d

---
+++ backjoon/24262.cpp
@@ -0,0 +1,13 @@
+//#include <iostream>
+//#include <vector>
+//#include <algorithm> 
+//#include<string>
+//#include <numeric>
+//using namespace std;
+//
+//int main()
+//{
+//	int a;
+//	cin >> a;
+//	cout << 1 << endl << 0;
+//}
