14a2ddbcd743f5f144f4fb30843558c64795fc41
euntaek Heo <gjdmsxor@inu.ac.kr>
2023-06-19 11:40:55 +0900

    d

---
+++ backjoon/2839.cpp
@@ -0,0 +1,37 @@
+//#include <iostream>
+//#include <vector>
+//#include <algorithm> 
+//#include <string>
+//#include <numeric>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	int ori = n;
+//	int cnt = 0;
+//	if (n ==4) {
+//		cout << -1;
+//		return 0;
+//	}
+//
+//	while (n >= 5) {
+//		n -= 5;
+//		cnt++;
+//		if (n == 6)break;
+//	}
+//	if (n <= 2||(n>=4&&n<=5)||n==7) {
+//		n = ori;
+//		cnt = 0;
+//	}
+//
+//	while (n >= 3) {
+//		n -=3;
+//		cnt++;
+//	}
+//	cout << cnt;
+//	
+//
+//
+//}
