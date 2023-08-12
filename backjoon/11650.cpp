96489c63b7c77eb208f0a1b166345f2c395bee0b
euntaek Heo <gjdmsxor@inu.ac.kr>
2023-08-12 20:26:12 +0900

    d

--- backjoon/11650.cpp
+++ backjoon/11650.cpp
@@ -1,81 +1,33 @@
-//#include <iostream>
-//#include <vector>
-//#include <algorithm> 
-//#include <string>
-//#include <numeric>
-//#include<cmath>
-//#include <sstream>
-//#include <ctime>
-//
-//using namespace std;
-//
-//class Point {
-//public:
-//	int x;
-//	int y;
-//
-//	Point() {}
-//	Point(int x, int y) {
-//		this->x = x;
-//		this->y = y;
-//	}
-//};
-//int main()
-//{
-//	clock_t start, finish;
-//	double duration;
-//
-//	start = clock();
-//
-//	/*실행 시간을 측정하고 싶은 코드*/
-//
-//	
-//
-//
-//	int number;
-//	cin >> number;
-//	vector<Point> v;
-//	for (int i = 0; i < number; i++) {
-//		int a, b;
-//		cin >> a >> b;
-//		Point* p = new Point;
-//		p->x = a;
-//		p->y = b;
-//		v.push_back(*p);
-//
-//	}
-//	cout << endl;
-//	//정렬 - x만 비교  만약 x가 같다 -> y비교
-//
-//
-//	
-//
-//
-//	for (int x = 0; x < v.size()-1; x++) {
-//		for (int y = x+1; y < v.size(); y++) {
-//			if (v[x].x > v[y].x) {
-//				Point temp = v[y];
-//				v[y] = v[x];
-//				v[x] = temp;
-//			}
-//			else if (v[x].x == v[y].x) {
-//				if (v[x].y > v[y].y) {
-//					Point temp = v[y];
-//					v[y] = v[x];
-//					v[x] = temp;
-//				}
-//			}
-//		}
-//	}
-//	for (int i = 0; i < number; i++) {
-//		cout << v[i].x << " " << v[i].y << endl;
-//	}
-//
-//
-//	finish = clock();
-//
-//	duration = (double)(finish - start) / CLOCKS_PER_SEC;
-//	cout << duration << "초" << endl;
-//}
-//
-//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+
+int comp(pair<int,int> a, pair<int,int> b) {
+	if (a.first == b.first) return a.second < b.second;
+	return a.first < b.first;
+}
+int main()
+{
+
+	vector<pair<int, int>>v;
+	int count;
+	cin >> count;
+	int a, b;
+	for (int i = 0; i < count; i++) {
+		
+		cin >> a >> b;
+		v.push_back(pair<int, int>(a, b));
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0; i < count; i++) {
+		cout << v[i].first <<" "<< v[i].second<< endl;;
+	}
+
+	
+}
+
+
--- backjoon/삽입.cpp
+++ backjoon/삽입.cpp
@@ -1,49 +1,44 @@
-#include <iostream>
-#include <vector>
-#include <algorithm> 
-#include <string>
-#include <numeric>
-#include<cmath>
-#include <sstream>
-#include <ctime>
-
-using namespace std;
-
-
-int main()
-{
-	int number;
-	cin >> number;
-	int y;
-	vector<int>v;
-	for (int i = 0; i < number; i++) {
-		int a;
-		cin >> a;
-		v.push_back(a);
-	}
-	//삽입정렬
-	int tmp;
-	int save;
-	for (int x = 1; x < v.size(); x++) {
-		tmp = v[x];  //임시변수
-		for (int y = x - 1; y >= 0; y--) {  //비교당할 수 
-			if (tmp < v[y]) {
-				v[y + 1] = v[y];
-			}
-			else break;
-		}
-
-			v[y + 1] = tmp;
-
-
-			
-		}
-
-
-		for (auto i : v) {
-			cout << i << " ";
-
-		}
-
-		return 0;
-	}
+//#include <iostream>
+//#include <vector>
+//#include <algorithm> 
+//#include <string>
+//#include <numeric>
+//#include<cmath>
+//#include <sstream>
+//#include <ctime>
+//
+//using namespace std;
+//
+//
+//int main()
+//{
+//	int number;
+//	cin >> number;
+//	int y;
+//	vector<int>v;
+//	for (int i = 0; i < number; i++) {
+//		int a;
+//		cin >> a;
+//		v.push_back(a);
+//	}
+//	//삽입정렬
+//	int tmp;
+//	for (int x = 1; x < v.size(); x++) {
+//		tmp = v[x];  //임시변수
+//		for (y = x - 1; y >= 0; y--) {  //비교당할 수 
+//			if (tmp < v[y]) {
+//				v[y + 1] = v[y];
+//			}
+//			else break;
+//		}
+//		v[y + 1] = tmp;
+//	}
+//
+//
+//	for (auto i : v) {
+//		cout << i << " ";
+//
+//	}
+//
+//	return 0;
+//}
