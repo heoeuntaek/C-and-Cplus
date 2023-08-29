ee0f968338f08f73cfe94eabea8b9b2d88b165f1
euntaek Heo <gjdmsxor@inu.ac.kr>
2023-08-29 23:26:41 +0900

    d

--- backjoon/2869.cpp
+++ backjoon/2869.cpp
@@ -1,54 +1,54 @@
-#include <iostream>
-#include <vector>
-#include <algorithm> 
-#include <string>
-#include <numeric>
-#include <cmath>
-#include <sstream>
-#include <tuple>
-
-using namespace std;
-
-
-int main()
-{
-	
-	int a, b, v;
-	cin >> a >> b >> v;
-	int day = 0;
-
-	day = ceil((double)v / ((double)(a - b)));
-	int now = day * (a - b);
-	/*int pre = now + 2 * b - 2*a;
-	while (pre+a >= v) {
-		pre = pre + 2 * b - 2*a;
-		day--;
-
-	}*/
-
-	//거꾸로
-	while (1) {
-		if (now + 2 * b - a < v) {
-			cout << day;
-			return 0;
-		}
-		now += b;
-		if (now <= v) {
-			cout << day;
-			return 0;
-		}
-
-		now = now - a;
-		day--;
-
-
-	}
-
-
-	cout << day;
-
-	return 0;
-
-}
-
-
+//#include <iostream>
+//#include <vector>
+//#include <algorithm> 
+//#include <string>
+//#include <numeric>
+//#include <cmath>
+//#include <sstream>
+//#include <tuple>
+//
+//using namespace std;
+//
+//
+//int main()
+//{
+//	
+//	int a, b, v;
+//	cin >> a >> b >> v;
+//	int day = 0;
+//
+//	day = ceil((double)v / ((double)(a - b)));
+//	int now = day * (a - b);
+//	/*int pre = now + 2 * b - 2*a;
+//	while (pre+a >= v) {
+//		pre = pre + 2 * b - 2*a;
+//		day--;
+//
+//	}*/
+//
+//	//거꾸로
+//	while (1) {
+//		if (now + 2 * b - a < v) {
+//			cout << day;
+//			return 0;
+//		}
+//		now += b;
+//		if (now <= v) {
+//			cout << day;
+//			return 0;
+//		}
+//
+//		now = now - a;
+//		day--;
+//
+//
+//	}
+//
+//
+//	cout << day;
+//
+//	return 0;
+//
+//}
+//
+//
--- backjoon/3009.cpp
+++ backjoon/3009.cpp
@@ -1,75 +1,77 @@
-//#include <iostream>
-//#include <vector>
-//#include <algorithm> 
-//
-//using namespace std;
-//
-//class Point
-//{
-//
-//public:
-//
-//
-//	int x;
-//	int y;
-//
-//	Point(int _x, int _y) :x(_x), y(_y) {};
-//	int getX() { return x; }
-//	int getY() { return y; }
-//	//Point(int _x, int _y) {
-//	//	x = _x;
-//	//	y = _y;
-//	//}
-//	void print() {
-//		cout << x <<" "<<y << endl;
-//	}
-//};
-//
-//
-//int main()
-//{
-//	/*1. 공통되는 것 찾음
-//		2. (각각 - 공통)을 공통에서 더함*/
-//
-//	Point *arr[4];
-//	//arr[0], arr[1],arr[2]
-//
-//	for (int i = 0; i < 3; i++) {
-//		int x, y;
-//		cin >> x >> y;
-//		arr[i] = new Point(x, y);
-//	}
-//	int index;
-//	int x, y;
-//	for (int i = 0; i < 3; i++) {
-//		if (arr[i]->getX() == arr[i]->getY()) {
-//			index = i;
-//			x = arr[i]->getX();
-//			y = arr[i]->getY();
-//		}
-//		
-//	}
-//	for (int i = 0; i < 3; i++) {
-//		if (arr[i]->getX() != x) {
-//			x +=arr[i]->getX() - x;
-//			break;
-//		}
-//	}
-//
-//	for (int i = 0; i < 3; i++) {
-//		if (arr[i]->getY() != y) {
-//			y +=arr[i]->getY() - y;
-//			break;
-//		}
-//	}
-//
-//	cout << x <<" " << y;
-//	
-//	
-//
-//
-//
-//
-//
-//	return 0;
-//}
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+#include <string>
+#include <numeric>
+#include <cmath>
+#include <sstream>
+#include <tuple>
+
+using namespace std;
+
+class Point
+{
+
+public:
+
+
+	int x;
+	int y;
+
+	Point(int _x, int _y) :x(_x), y(_y) {};
+	int getX() { return x; }
+	int getY() { return y; }
+	//Point(int _x, int _y) {
+	//	x = _x;
+	//	y = _y;
+	//}
+	void print() {
+		cout << x << " " << y << endl;
+	}
+};
+
+
+int main()
+{
+	/*1. 공통되는 것 찾음
+		2. (각각 - 공통)을 공통에서 더함*/
+
+	Point* arr[4];
+	//arr[0], arr[1],arr[2], arr[3]
+
+	for (int i = 0; i < 3; i++) {
+		int x, y;
+		cin >> x >> y;
+		arr[i] = new Point(x, y);
+	}
+	int max, next;
+	vector<int>v;
+
+	for (int i = 0; i < 3; i++) {
+		v.push_back(arr[i]->getX());
+		v.push_back(arr[i]->getY());
+	}
+	sort(v.begin(), v.end());
+
+	int a, b;
+	for (auto i : v) {
+		a = i;
+		if (a == i) {
+			b = i;
+		}
+	}
+
+	for (int i = 0; i < 3; i++) {
+		int x = arr[i]->getX();
+		int y = arr[i]->getY();
+		if (x == a or x == b) {
+			if (y == a or y == b) {
+
+			}
+		}
+	}
+
+
+
+	return 0;
+}
