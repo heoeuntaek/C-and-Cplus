bff0be765a75dfa19eb1cc86d61b8a59511dfa32
euntaek Heo <gjdmsxor@inu.ac.kr>
2023-09-05 21:27:47 +0900

    d

--- backjoon/24266.cpp
+++ backjoon/24266.cpp
@@ -1,31 +1,31 @@
-#include <iostream>
-#include <vector>
-#include <algorithm> 
-#include <string>
-#include <numeric>
-#include <cmath>
-#include <sstream>
-#include <tuple>
-
-using namespace std;
-
-
-
-
-
-int main()
-{
-
-
-	long long n;
-	cin >> n;
-	int cnt = 0;
-	
-
-
-
-	cout << n *n*n << "\n" << 3;
-
-
-	return 0;
-}
+//#include <iostream>
+//#include <vector>
+//#include <algorithm> 
+//#include <string>
+//#include <numeric>
+//#include <cmath>
+//#include <sstream>
+//#include <tuple>
+//
+//using namespace std;
+//
+//
+//
+//
+//
+//int main()
+//{
+//
+//
+//	long long n;
+//	cin >> n;
+//	int cnt = 0;
+//	
+//
+//
+//
+//	cout << n*(n-1)*(n-2)/6 << "\n" << 3;
+//
+//
+//	return 0;
+//}
--- backjoon/2839.cpp
+++ backjoon/2839.cpp
@@ -1,37 +1,33 @@
-//#include <iostream>
-//#include <vector>
-//#include <algorithm> 
-//#include <string>
-//#include <numeric>
-//using namespace std;
-//
-//int main()
-//{
-//	int n;
-//	cin >> n;
-//	int ori = n;
-//	int cnt = 0;
-//	if (n ==4) {
-//		cout << -1;
-//		return 0;
-//	}
-//
-//	while (n >= 5) {
-//		n -= 5;
-//		cnt++;
-//		if (n == 6)break;
-//	}
-//	if (n <= 2||(n>=4&&n<=5)||n==7) {
-//		n = ori;
-//		cnt = 0;
-//	}
-//
-//	while (n >= 3) {
-//		n -=3;
-//		cnt++;
-//	}
-//	cout << cnt;
-//	
-//
-//
-//}
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+#include <string>
+#include <numeric>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int count = 0;
+	if (n == 1 or n == 2 or n == 4 or n == 7) {
+		cout << -1;
+		return 0;
+	}
+	while (n>0) {
+
+		n -= 5;
+		count++;
+		if (n == 1 or n == 2 or n == 4 or n == 7 or n<0) {
+			n += 5;
+			n -= 3;
+		}
+	}
+	cout<<count;
+
+
+	return 0;
+
+
+
+}
